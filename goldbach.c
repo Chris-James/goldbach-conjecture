4107829193b9bcf1aa0b1234c89cc9e71335260b
Chris James <chris@chris-james.me>
2016-01-28 08:59:22 -0500

    Initial commit

---
+++ goldbach.c
@@ -0,0 +1,16 @@
+/*
+Goldbach's Conjecture
+
+"Every even number greater than or equal to 4 can be expressed as the sum of two prime numbers."
+
+Verify Goldbach's conjecture as expressed by Euler for any even numbers less than a million submitted by user.
+
+Author: Chris Impastato
+*/
+
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+
+  return 0;
+}
